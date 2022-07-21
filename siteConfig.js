ef35884c1f5d2d41e1e279ebacfd1d33b8b4a075
boazglick <95533522+boazglick@users.noreply.github.com>
2022-07-21 19:01:17 +0300

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -1,69 +1,63 @@
 module.exports = {
-  siteUrl: "https://wp-balsa-preview.draftbox.co", // Site domain. Do not include a trailing slash!
+  siteUrl: "https://tikvahadasha.co.il", // Site domain. Do not include a trailing slash!
 
-  postsPerPage: 10, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
+  postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-  siteTitleMeta: "Built with Draftbox", // This allows an alternative site title for meta data for pages.
+  siteTitleMeta: "tikvahadasha", // This allows an alternative site title for meta data for pages.
   siteDescriptionMeta:
-    "Lightning fast, secure front-end for your WordPress or Ghost blog, without coding", // This allows an alternative site description for meta data for pages.
+    "tikvahadasha", // This allows an alternative site description for meta data for pages.
 
   shareImageWidth: 1000, // Change to the width of your default share image
   shareImageHeight: 523, // Change to the height of your default share image
 
-  shortTitle: "Built with Draftbox", // Used for App manifest e.g. Mobile Home Screen
+  shortTitle: "tikvahadasha", // Used for App manifest e.g. Mobile Home Screen
   siteIcon: "favicon.png", // Logo in /static dir used for SEO, RSS, and App manifest
   backgroundColor: "#e9e9e9", // Used for Offline Manifest
   themeColor: "#15171A", // Used for Offline Manifest
   apiUrl: "https://wordpress.theasdfghjkl.com",
   subscribeWidget: {
     visible: true,
-    title: "Subscribe to Draftbox",
-    helpText: "Get the latest posts delivered right to your inbox.",
-    successMessage: "Thanks for subscribing to Draftbox.",
+    title: "tikvahadasha",
+    helpText: "הישארו מעודכנים",
+    successMessage: "תודה ההודעה נשלחה בהצלחה",
   },
   header: {
     navigation: [
       {
-        label: "Home",
-        url: "https://wp-balsa-preview.draftbox.co/",
+        label: "דף הבית",
+        url: "https://tikvahadasha.co.il/",
       },
       {
-        label: "Contact",
-        url: "https://wp-balsa-preview.draftbox.co/contact",
+        label: "יצירת קשר",
+        url: "https://tikvahadasha.co.il/contact",
       },
     ],
   },
   footer: {
-    copyright: "Built with Draftbox",
+    copyright: "כל הזכויות שמורות ",
     navigation: [
       {
-        label: "Home",
-        url: "https://wp-balsa-preview.draftbox.co/",
+        label: "דף הבית",
+        url: "https://tikvahadasha.co.il/",
       },
       {
         label: "Sitemap",
-        url: "https://wp-balsa-preview.draftbox.co/sitemap.xml",
+        url: "https://tikvahadasha.co.il/sitemap.xml",
       },
+      
       {
-        label: "RSS",
-        url: "https://wp-balsa-preview.draftbox.co/rss.xml",
+        label: "יצירת קשר",
+        url: "https://tikvahadasha.co.il/contact",
       },
-      {
-        label: "Contact",
-        url: "https://wp-balsa-preview.draftbox.co/contact",
-      },
-      {
-        label: "External Link",
-        url: "https://spectrum.chat/gatsby-js/themes?tab=posts",
-      },
+     
     ],
   },
   socialLinks: {
-    twitter: "https://twitter.com/draftboxhq",
-    facebook: "https://facebook.com/",
-    instagram: "https://www.instagram.com/",
-    linkedin: "https://linkedin.com/",
-    github: "https://github.com/draftbox-co",
+    twitter: "",
+    facebook: "",
+    instagram: "",
+    linkedin: "",
+    github: "",
     pinterest: "",
     youtube: "",
     dribbble: "",
@@ -72,18 +66,18 @@ module.exports = {
     whatsapp: "",
   },
   contactWidget: {
-    title: "Contact Built with Draftbox",
+    title: "https://tikvahadasha.co.il/",
     successMessage: "We’ll get in touch with you soon.",
   },
   metadata: {
-    title: "Built with Draftbox",
+    title: "goldko",
     description:
-      "Lightning fast, secure front-end for your WordPress or Ghost blog, without coding. Draftbox is a new-age blogging platform for everyone, built on Gatsby.",
+      "goldko",
   },
   twitterCard: {
-    title: "Built with Draftbox",
+    title: "",
     description:
-      "Lightning fast, secure front-end for your WordPress or Ghost blog, without coding. Draftbox is a new-age blogging platform for everyone, built on Gatsby.",
+      "",
     imageUrl: "twitterImage.png",
     username: "@DraftboxHQ",
   },
@@ -94,10 +88,11 @@ module.exports = {
     imageUrl: "facebookImage.png",
     appId: "2391725224459953",
   },
-  siteTitle: "Built with Draftbox",
+  siteTitle: "tikvahadasha",
   siteDescription:
-    "Lightning fast, secure front-end for your WordPress or Ghost blog, without coding.",
-  language: "en",
+    "tikvahadasha",
+  language: "he",
+  direction: "rtl",
   logoUrl: "logo.png",
   iconUrl: "favicon.png",
   coverUrl: "cover.jpg",
@@ -106,62 +101,27 @@ module.exports = {
     variables: [
       {
         varName: "--primary-color",
-        value: `#2b6cb0`,
+        value: `#ff0030`,
       },
       {
         varName: "--primary-color-active",
-        value: `#2a4365`,
+        value: `#ff0030`,
       },
       {
         varName: "--primary-color-light",
-        value: `#bee3f8`,
+        value: `#ff0030`,
       },
-      {
-        varName: "--sans-font",
-        value: `"Montserrat"`,
-      },
-      {
-        varName: "--sans-font-normal",
-        value: `400`,
-      },
-      {
-        varName: "--sans-font-medium",
-        value: `500`,
-      },
-      {
-        varName: "--sans-font-semibold",
-        value: `600`,
-      },
-      {
-        varName: "--sans-font-bold",
-        value: `700`,
-      },
-      {
-        varName: "--serif-font",
-        value: `Merriweather`,
-      },
-      { varName: "--serif-font-light", value: `300` },
-      { varName: "--serif-font-normal", value: `400` },
-      { varName: "--serif-font-medium", value: `400` },
-      { varName: "--serif-font-bold", value: `700` },
+     
+    
     ],
-    fonts: [
-      {
-        family: "Montserrat",
-        variants: ["400", "500", "600", "700"],
-        //subsets: ['latin']
-        //text: 'Hello'
-        fontDisplay: "swap",
-        strategy: "selfHosted", // 'base64' || 'cdn'
-      },
-      {
-        family: "Merriweather",
-        variants: ["300", "400", "700"],
-        //subsets: ['latin']
-        //text: 'Hello'
-        fontDisplay: "swap",
-        strategy: "selfHosted", // 'base64' || 'cdn'
-      },
-    ],
+    	 stylesheets: [
+    'https://tikvahadasha.ussl.co.il/style.css',
+    {
+      href: 'https://tikvahadasha.ussl.co.il/style.css',
+      type: 'text/css',
+    },
+  ],
+
+   
   },
 };
